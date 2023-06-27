64a1eb8dae694eca709f3863d2bc2c2b5b60d212
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2023-06-27 01:48:14 -0300

    feat/uploadSv

--- functions/app.js
+++ functions/app.js
@@ -21,7 +21,7 @@ var app = express();
 app.use(cors())
 
 // view engine setup
-app.set('views', path.join(__dirname, '../views'));
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
