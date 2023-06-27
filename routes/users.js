767896ce3d9cc5a00b04297b105b43ada6e54811
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2023-06-27 02:19:11 -0300

    feat/Prod

--- routes/users.js
+++ routes/users.js
@@ -5,4 +5,8 @@ const controllers = require('../controllers/usersControllers')
 router.post('/register', controllers.create)
 router.post('/login', controllers.login)
 
+router.get('/', (req,res)=>{
+    res.send('hol')
+})
+
 module.exports = router;
